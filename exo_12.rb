2461d16704edc8d702630f39d41d3dbad72a4445
crousty24-bit <allen.kch24@gmail.com>
2026-01-12 21:24:31 +0100

    add while loop counter to user num

---
+++ exo_12.rb
@@ -0,0 +1,7 @@
+puts "Donne moi un nombre"
+user_number = gets.chomp.to_i
+count = 1 #initialize counter
+while count < user_number #loop condition comparison, if True then :
+  puts "count= " + count.to_s #display counter w/ current value converted to str
+  count += 1 #increment counter by 1 each time loop runs
+end
