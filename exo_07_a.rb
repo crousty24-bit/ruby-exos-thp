801f7e587c49c8d0378878a0fc8d972b3edd0219
crousty24-bit <allen.kch24@gmail.com>
2026-01-12 19:49:40 +0100

    add 2 variants to test input

--- exo_07_a.rb
+++ exo_07_a.rb
@@ -1,3 +1,11 @@
 puts "Bonjour, c'est quoi ton blase ?"
 user_name = gets.chomp #method to get/read user input ; .chomp removes newline character \n
-puts "Hello #{user_name}!" 
+puts "Hello #{user_name}!"
+#second program
+puts "Bonjour, c'est quoi ton blase ?"
+print "> "
+user_name = gets.chomp
+puts user_name
+#third program
+user_name = gets.chomp
+puts user_name
